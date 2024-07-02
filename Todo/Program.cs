d6c7d58e6627a8e8d17581783b24004f3b6e1da6
Kash Bleem <kash.bleem@gmail.com>
2024-07-02 14:50:32 +0100

    Task 5: Checkbox created to hide items marked as IsDone / completed. Included a build helper to recreate my user incase i need to nuke the DB for reasons. Also includes bugfix for point 4! Silly me...

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Todo.Models; // Ensure this namespace includes ApplicationUser
 
 namespace Todo
 {
@@ -14,11 +12,43 @@ namespace Todo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            CreateUserIfNotExists(host);
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        private static void CreateUserIfNotExists(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+
+                    var user = new IdentityUser { UserName = "kash.bleem@gmail.com", Email = "kash.bleem@gmail.com" };
+                    var result = userManager.CreateAsync(user, "Password123!").Result;
+
+                    if (result.Succeeded)
+                    {
+                        // Assign roles??
+                    }
+                    else
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogWarning("User creation failed: {0}", string.Join(", ", result.Errors.ToString()));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while creating the user.");
+                }
+            }
+        }
     }
 }
