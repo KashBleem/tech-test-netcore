bc0807fe04eb6c75cc2c2e083526cb224ebbad0e
Kash Bleem <kash.bleem@gmail.com>
2024-07-02 20:47:01 +0100

    Task 9: Reworked Detail view and added an API endpoint in TodoItemController. Would prefer this to be separated out entirely in a separate service for API endpoint. Would also have preferred to create a partial view / component for the "create item" within Detail view.

--- Todo/Controllers/TodoItemController.cs
+++ Todo/Controllers/TodoItemController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Todo.Data;
 using Todo.Data.Entities;
 using Todo.EntityModelMappers.TodoItems;
@@ -34,12 +35,9 @@ namespace Todo.Controllers
         public async Task<IActionResult> Create(TodoItemCreateFields fields)
         {
             if (!ModelState.IsValid) { return View(fields); }
-
             var item = new TodoItem(fields.TodoListId, fields.ResponsiblePartyId, fields.Title, fields.Importance, fields.Rank);
-
             await dbContext.AddAsync(item);
             await dbContext.SaveChangesAsync();
-
             return RedirectToListDetail(fields.TodoListId);
         }
 
@@ -56,25 +54,64 @@ namespace Todo.Controllers
         public async Task<IActionResult> Edit(TodoItemEditFields fields)
         {
             if (!ModelState.IsValid) { return View(fields); }
-
             var todoItem = dbContext.SingleTodoItem(fields.TodoItemId);
-
             TodoItemEditFieldsFactory.Update(fields, todoItem);
-
             dbContext.Update(todoItem);
             await dbContext.SaveChangesAsync();
-
             return RedirectToListDetail(todoItem.TodoListId);
+        }
+
+        //full disclosure, if i had time i would MUCH rather stick this in its own Swagger API
+        [HttpPost]
+        [Route("api/TodoItem/Create")]
+        public async Task<IActionResult> CreateApi([FromBody] CreateTodoItemRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoList = dbContext.SingleTodoList(request.TodoListId);
+            var fields = TodoItemCreateFieldsFactory.Create(todoList, User.Id(), todoList.Owner);
+            fields.Title = request.Title;
+            fields.Importance = request.Importance;
+            fields.Rank = request.Rank;
+
+            var item = new TodoItem(fields.TodoListId, fields.ResponsiblePartyId, fields.Title, fields.Importance, fields.Rank);
+            await dbContext.AddAsync(item);
+            await dbContext.SaveChangesAsync();
+
+            var responsibleParty = await dbContext.Users.FindAsync(fields.ResponsiblePartyId);
+            var gravatarProfile = await GetGravatarProfile(responsibleParty.Email);
+
+            var viewModel = new TodoItemSummaryViewmodel(
+                item.TodoItemId,
+                item.Title,
+                item.IsDone,
+                new UserSummaryViewmodel(responsibleParty.UserName, responsibleParty.Email),
+                item.Importance,
+                item.Rank
+            );
+
+            return Ok(viewModel);
         }
 
         private RedirectToActionResult RedirectToListDetail(int fieldsTodoListId)
         {
-            return RedirectToAction("Detail", "TodoList", new {todoListId = fieldsTodoListId});
+            return RedirectToAction("Detail", "TodoList", new { todoListId = fieldsTodoListId });
         }
 
         private async Task<GravatarProfile> GetGravatarProfile(string email)
         {
             return await _gravatarService.GetProfileDetailsAsync(email);
         }
+    }
+
+    public class CreateTodoItemRequest
+    {
+        public string Title { get; set; }
+        public Importance Importance { get; set; }
+        public int Rank { get; set; }
+        public int TodoListId { get; set; }
     }
 }
